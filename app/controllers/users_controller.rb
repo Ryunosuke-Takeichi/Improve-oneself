76b61e97904427ee5e46ccab5a1f03e302b3f0fa
EC2 Default User <ec2-user@ip-172-31-82-121.ec2.internal>
2020-12-07 10:18:43 +0000

    User

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,30 @@
 class UsersController < ApplicationController
+    
+  def index
+    @users = User.all
+  end
+  
+  def show
+    @user = User.find(params[:id])
+  end
+  
+  def edit
+    @user = User.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    if 
+     redirect_to user_path(@user.id)
+    else
+     render :edit
+    end
+  end
+  
+  private 
+  def user_params
+    params.require(:user).permit(:name, :introduction, :height, :weight, :profile_image)
+  end
+  
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '9ed76f55e9c28a058ab19cfa4dcee30722d9b4177bd377497cd659b3996365e0202b942660c921813c71568df137b3c7bab9082f509b981d0c0f79bb4c0a7167'
